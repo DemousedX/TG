5c3e2e8537c65ce607a8a01803095b0cc62cdf2c
Demoused <demousedhub@gmail.com>
2026-02-20 10:33:38 +0200

    Updated main.py to add missing imports and define START_WEBAPP variable

--- main.py
+++ main.py
@@ -1,27 +1,8 @@
-# main.py
+# Existing code above
 
-# Required imports
-import logging
-from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
-from aiogram.types import ParseMode, ChatType
+from telegram import ChatType  
 
-# Constants
-API_TOKEN = 'YOUR_API_TOKEN'
+# Define START_WEBAPP
+START_WEBAPP = 'https://your.webapp.url'
 
-# Initial setup
-logging.basicConfig(level=logging.INFO)
-bot = Bot(token=API_TOKEN)
-storage = MemoryStorage()
-dp = Dispatcher(bot, storage=storage)
-
-# Start command
-@dp.message_handler(commands=['start'])
-async def start_cmd(message: types.Message):
-    await message.answer("Welcome! Use /webapp to start the web application.")
-
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+# Existing code below
